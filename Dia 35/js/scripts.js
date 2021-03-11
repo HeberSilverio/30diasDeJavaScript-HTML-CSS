e16af251b5046a10b5af46b8280749bf37dc1b8c
Héber Silvério <hebersilverio@gmail.com>
2021-03-10 21:00:12 -0300

    conclui código do dia 35

--- Dia 35/js/scripts.js
+++ Dia 35/js/scripts.js
@@ -1,5 +1,37 @@
+
 $(function(){
-    $('.watch-slider').slick({
-        
+
+    function atualizar_informacoes() {
+      $("#watch-name").text( $(".slick-center").data("name") );
+      var watchPrice = parseFloat($(".slick-center").data("price")).toLocaleString("pt-BR", {style:"currency", currency: "BRL", minimumFractionDigits: 2});
+      $("#watch-price").text( watchPrice );
+    }
+  
+    $(".watch-slider").on('init', function(){
+      atualizar_informacoes();
     });
-})
+  
+    $(".watch-slider").slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: true,
+      prevArrow: $("#arrow-prev"),
+      nextArrow: $("#arrow-next"),
+      responsive: [
+        {
+          breakpoint: 640,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  
+    $(".watch-slider").on('afterChange', function(){
+      atualizar_informacoes();
+    });
+  
+  })
+  
+  
